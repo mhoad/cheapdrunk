dc2d09aba6671ca35b15bcfe9c3850c787619fa2
Mark Hoad <markdhoad@gmail.com>
2012-05-01 20:40:53 +1000

    Added edit option for venue

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -19,11 +19,24 @@ def create
 end
 
 def update
-	
+	@venue = Venue.find(params[:id])
+  if @venue.update_attributes(params[:venue])
+    flash[:success] = "Venue updated"
+    redirect_to @venue
+  else
+    render 'edit'
+  end
 end
+
+def edit
+    @venue = Venue.find(params[:id])
+end
+
 
 def destroy
 	
 end
+
+
 
 end
