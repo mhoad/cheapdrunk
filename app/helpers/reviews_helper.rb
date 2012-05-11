ad19686e3a9c7015057a62f56f0afb0f0e3439cc
Mark Hoad <markdhoad@gmail.com>
2012-05-11 22:32:30 +1000

    Added star ratings to venue index

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,14 +1,8 @@
 module ReviewsHelper
   def display_stars(star)
-    output = ""
-    for i in 1..5
-      if i.eql? star
-        output << radio_button_tag('star3',nil, true, :class => 'star', :disabled => true)
-      else
-        output << radio_button_tag('star3',nil, false, :class => 'star', :disabled => true)
-      end
-    end
-    output.html_safe
+    (1..5).inject("") do |output, i|
+      output << radio_button_tag('star3',nil, i.eql?(star), :class => 'star', :disabled => true)
+    end.html_safe
   end
 
   def average_rating(venue_id)
