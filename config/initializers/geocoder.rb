69be0e88e1bdde8b927b03fa49b6381e0746b1b3
Mark Hoad <markdhoad@gmail.com>
2012-05-15 16:36:48 +1000

    Possible fix for Geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,17 +1,7 @@
 # config/initializers/geocoder.rb
 
 # geocoding service (see below for supported options):
-Geocoder::Configuration.lookup = :google_premier
-
-# to use an API key:
-Geocoder::Configuration.api_key = "AIzaSyB2l6Sc_i5guELQZ_g_KMbEfkt4WZwR03k"
+Geocoder::Configuration.lookup = :google
 
 # geocoding service request timeout, in seconds (default 3):
 Geocoder::Configuration.timeout = 5
-
-# use HTTPS for geocoding service connections:
-Geocoder::Configuration.use_https = false
-
-
-# language to use (for search queries and reverse geocoding):
-Geocoder::Configuration.language = "en-AU"
